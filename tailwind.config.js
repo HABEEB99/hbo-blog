96804d6f06bb122fe8b6f9cb97175ff66ed9d80e
habeeb-ahmadu <habeebahmadu1@gmail.com>
2022-07-14 10:56:57 +0100

    installed Next.js and other dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: 'jit',
+	darkMode: 'class',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {},
+		},
+	},
+	plugins: [require('tailwind-scrollbar'), require('tailwind-scrollbar-hide')],
+};
